603505e7be4b947809ca2b12b9a0a730e9d3e3c8
Staijn1 <stein@jnkr.eu>
2021-05-25 23:27:03 +0200

    Checkbox renderer gefixt & css bootstrap teruggezet

--- src/app/shared/components/datatable/checkbox-render/checkbox-render.component.ts
+++ src/app/shared/components/datatable/checkbox-render/checkbox-render.component.ts
@@ -1,26 +1,29 @@
 import {Component, OnInit} from '@angular/core';
-import {AgRendererComponent} from "ag-grid-angular";
-import {ICellRendererParams} from "ag-grid-community";
+import {AgRendererComponent} from 'ag-grid-angular';
+import {ICellRendererParams} from 'ag-grid-community';
+import {faCheck, faTimes} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-    selector: 'app-checkbox-render',
-    templateUrl: './checkbox-render.component.html',
-    styleUrls: ['./checkbox-render.component.scss']
+  selector: 'app-checkbox-render',
+  templateUrl: './checkbox-render.component.html',
+  styleUrls: ['./checkbox-render.component.scss']
 })
 export class CheckboxRenderComponent implements AgRendererComponent {
-    private boolWaarde: boolean = false;
+  boolWaarde: boolean = false;
+  check = faCheck;
+  cross = faTimes;
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    agInit(params: ICellRendererParams): void {
-        if ((params.value === 1) || (params.value === "1") || (params.value == true)) {
-            this.boolWaarde = true;
-        }
+  agInit(params: ICellRendererParams): void {
+    if ((params.value === 1) || (params.value === '1') || (params.value == true)) {
+      this.boolWaarde = true;
     }
+  }
 
-    refresh(params: ICellRendererParams): boolean {
-        return false;
-    }
+  refresh(params: ICellRendererParams): boolean {
+    return false;
+  }
 
 }
