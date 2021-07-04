504d628fc68663c6c71503f5c5b3ce75bfc3e1f7
richard <richard@jnkr.eu>
2021-07-04 20:47:23 +0200

    event na update in helios database

--- src/app/services/apiservice/types.service.ts
+++ src/app/services/apiservice/types.service.ts
@@ -8,37 +8,37 @@ import {KeyValueString} from '../../types/Utils';
   providedIn: 'root'
 })
 export class TypesService {
-  types: HeliosTypes | null = null;
 
   constructor(private readonly apiService: APIService, private readonly storageService: StorageService) {
   }
 
   async getTypes(groep: number): Promise<HeliosType[]> {
+    let types: HeliosTypes | null = null;
     let hash: string = '';
 
     if (this.storageService.ophalen('types-'+groep) != null) {
-      this.types = this.storageService.ophalen('types-'+groep);
+      types = this.storageService.ophalen('types-'+groep);
     }
 
     let getParams: KeyValueString = {};
 
     getParams['GROEP'] = groep.toString();
 
-    if (this.types != null) { // we hebben eerder de lijst opgehaald
-      hash = this.types.hash as string;
+    if (types != null) { // we hebben eerder de lijst opgehaald
+      hash = hash as string;
       getParams['HASH'] = hash;
     }
 
     try {
       const response = await this.apiService.get('Types/GetObjects', getParams);
 
-      this.types = await response.json();
-      this.storageService.opslaan('types-'+groep, this.types);
+      types = await response.json();
+      this.storageService.opslaan('types-'+groep, types);
     } catch (e) {
       if (e.responseCode !== 304) { // server bevat dezelfde data als cache
         throw(e);
       }
     }
-    return this.types?.dataset as [];
+    return types?.dataset as [];
   }
 }
