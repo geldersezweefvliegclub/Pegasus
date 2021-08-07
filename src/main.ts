11e8566f60b5c80cc7554972574f71c0e933f661
richard <richard@jnkr.eu>
2021-08-07 10:15:16 +0200

    "ag-grid-enterprise": "^24.1.0" verwijderd

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
-import {LicenseManager} from 'ag-grid-enterprise';
-import {aggridLicense} from '../licenses';
+//import {LicenseManager} from 'ag-grid-enterprise';
+//import {aggridLicense} from '../licenses';
 
-LicenseManager.setLicenseKey(aggridLicense);
+//LicenseManager.setLicenseKey(aggridLicense);
 
 
 if (environment.production) {
