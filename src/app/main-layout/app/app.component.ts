5f1ad5673f58909a5fd0f68154aeaf570369f5cc
Stein Jonker <stein@jnkr.eu>
2023-07-01 13:48:24 +0000

    Hopefully fix scrolling issue

--- src/app/main-layout/app/app.component.ts
+++ src/app/main-layout/app/app.component.ts
@@ -9,7 +9,7 @@ import {StorageService} from "../../services/storage/storage.service";
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
-    styleUrls: ['app.component.css']
+    styleUrls: ['app.component.scss']
 })
 
 export class AppComponent {
