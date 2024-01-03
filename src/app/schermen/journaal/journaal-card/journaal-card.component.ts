b17ecd519a895c5b4c5e635785bf3a3da6797b5f
rjnkr <richard@jnkr.eu>
2024-01-03 19:42:39 +0100

    Journaal ook voor mobiel

---
+++ src/app/schermen/journaal/journaal-card/journaal-card.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {HeliosJournaalDataset} from "../../../types/Helios";
+import {SharedService} from "../../../services/shared/shared.service";
+import {faPenToSquare} from "@fortawesome/free-solid-svg-icons";
+import {JournaalEditorComponent} from "../../../shared/components/editors/journaal-editor/journaal-editor.component";
+
+@Component({
+    selector: 'app-journaal-card',
+    templateUrl: './journaal-card.component.html',
+    styleUrls: ['./journaal-card.component.scss']
+})
+export class JournaalCardComponent implements OnInit {
+    @Input() melding: HeliosJournaalDataset;
+    @ViewChild(JournaalEditorComponent) editor: JournaalEditorComponent;
+
+    protected readonly iconEdit = faPenToSquare;
+    datumDM: string
+    constructor(private readonly sharedService: SharedService) {
+    }
+
+    ngOnInit(): void {
+        this.datumDM = this.sharedService.datumDM(this.melding.DATUM!)    // jaar hoeft niet getoond te worden
+    }
+
+    openEditor() {
+        this.editor.openPopup(this.melding as HeliosJournaalDataset);
+    }
+}
