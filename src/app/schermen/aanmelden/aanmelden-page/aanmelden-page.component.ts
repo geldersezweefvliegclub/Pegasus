4346b67c3ee8075816046ee94c22e946acdab1d6
rjnkr <richard@jnkr.eu>
2024-05-05 09:17:21 +0200

    donateurs kunnen ook DDWV vliegen

--- src/app/schermen/aanmelden/aanmelden-page/aanmelden-page.component.ts
+++ src/app/schermen/aanmelden/aanmelden-page/aanmelden-page.component.ts
@@ -559,6 +559,7 @@ export class AanmeldenPageComponent implements OnInit, OnDestroy {
                 case 603: // jeugdlid
                 case 604: // private owner
                 case 605: // veteraan
+                case 606: // donateur
                 {
                     if (!this.rooster[idx].CLUB_BEDRIJF && ui!.LidData!.STATUSTYPE_ID !== 1903) {  // 1903 = Brevethouder
                         console.log(dagDatum, "Geen clubdag, geen brevethouder");
