ca82d904274ef946cc58db55b42f475244850fd2
stein.jonker <stein@jnkr.eu>
2024-09-15 17:25:56 +0200

    Move to new configuration file format for eslint

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+module.exports = [
+  {
+    ignores: ["projects/**/*"]
+  },
+  {
+    files: ["*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: ["tsconfig.json", "e2e/tsconfig.json"],
+        createDefaultProgram: true
+      }
+    },
+    plugins: {
+      "@angular-eslint": require("@angular-eslint/eslint-plugin"),
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin")
+    },
+    rules: {
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          "prefix": "app",
+          "style": "kebab-case",
+          "type": "element"
+        }
+      ],
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          "prefix": "app",
+          "style": "camelCase",
+          "type": "attribute"
+        }
+      ]
+    }
+  },
+  {
+    files: ["*.html"],
+    plugins: {
+      "@angular-eslint/template": require("@angular-eslint/eslint-plugin-template")
+    },
+    rules: {}
+  }
+];
