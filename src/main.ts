5e183f1f576ab36b8659ee05cc165be613d0af6d
Staijn <stein@jnkr.eu>
2026-08-15 22:38:40 +0200

    Upgrade to Angular v20. - Applied migration to use new control-flow syntax - Updated some packages to work around peer-dependency error

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {enableProdMode} from '@angular/core';
+import {platformBrowser} from '@angular/platform-browser';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
 /*
 import {LicenseManager} from 'ag-grid-enterprise';
@@ -15,5 +15,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowser().bootstrapModule(AppModule)
   .catch(err => console.error(err));
