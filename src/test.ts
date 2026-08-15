4fe59e2b77ae06efd22a10b65af3f91ecea91f36
Staijn <stein@jnkr.eu>
2026-08-15 23:27:58 +0200

    Fix tests not being able to run after update to V20

--- src/test.ts
+++ src/test.ts
@@ -2,10 +2,10 @@
 
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+    BrowserTestingModule,
+    platformBrowserTesting()
 );
